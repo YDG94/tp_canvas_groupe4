7a3d630027b3d2bb10ce9cb93de36d63741e9f51
bilelsaidi <bilelsaidi18@gmail.com>
2018-08-16 08:54:49 -0400

    html+css

--- script/main.js
+++ script/main.js
@@ -1,4 +1,6 @@
 "use strict";
+import {svgItems} from 'struct';
+
 console.log("js loaded");
 let myLeftDivSVG = document.querySelector("#svgContent");
 
@@ -7,22 +9,18 @@ myLeftDivSVG.addEventListener("click", function (evt) {
     console.log(evt.target);
 });
 
-let myItems =['bureau01.svg', 'bureau02.svg','bureau03.svg','canape01.svg','canape02.svg','canape03.svg','plante01.svg','plante02.svg','plante03.svg', 'table01.svg','table02.svg', 'table03.svg'];
-
 //test if browser siupports SVG!
 if (SVG.supported) {
     //if supported we move ahead and do our dirty jon on DOM loaded!
-    SVG.on(document, 'DOMContentLoaded', function() {
+    SVG.on(document, 'DOMContentLoaded', function () {
         console.log("DOM loaded!");
-    let leftSVGList = SVG('svgContent');
-        for (let i = 0; i < myItems.length; i++) {
-            leftSVGList.image(`images/${myItems[i]}`, "100%", "15vh").move(0, `${15*i}vh`);
+        let leftSVGList = SVG('svgContent');
+        for (let i = 0; i < svgItems.length; i++) {
+            leftSVGList.image(`images/${svgItems[i].imgSrc}`, "100%", "15vh").move(0, `${15 * i}vh`);
         }
-        leftSVGList.size("100%",`${myItems.length*15}vh`);
-        myLeftDivSVG.height= `${myItems.length*15}vh`;
+        leftSVGList.size("100%", `${svgItems.length * 15}vh`);
+        myLeftDivSVG.height = `${svgItems.length * 15}vh`;
     });
-
-
 } else {
     alert('SVG not supported');
 }
---
+++ script/struct.js
@@ -0,0 +1,50 @@
+"use strict";
+
+export class SvgElement {
+    constructor(elemId, elemName, elemDom, versId) {
+        this.elemId = elemId;
+        this.elemName = elemName;
+        this.elemDom = elemDom;
+        this.versId = versId;
+    }
+}
+
+export class Version {
+    constructor(numVers, dateCreation, projectId) {
+        this.numVers = numVers;
+        this.dateCreation = dateCreation;
+        this.projectId = projectId;
+    }
+}
+
+export class Project {
+    constructor(projetId, dateCreation, standImg, userId) {
+        this.projetId = projetId;
+        this.dateCreation = dateCreation;
+        this.standImg = standImg;
+        this.userId = userId;
+    }
+}
+
+export let tabCategories = [
+    {name: "All elements", value: "0"},
+    {name: "Bureau", value: "1"},
+    {name: "Canape", value: "2"},
+    {name: "Plante", value: "3"},
+    {name: "table", value: "4"}
+];
+
+export let svgItems = [
+    {imgSrc: "bureau01.svg", catValue: "1"},
+    {imgSrc: "bureau02.svg", catValue: "1"},
+    {imgSrc: "bureau03.svg", catValue: "1"},
+    {imgSrc: "canape01.svg", catValue: "2"},
+    {imgSrc: "canape02.svg", catValue: "2"},
+    {imgSrc: "canape03.svg", catValue: "2"},
+    {imgSrc: "plante01.svg", catValue: "3"},
+    {imgSrc: "plante02.svg", catValue: "3"},
+    {imgSrc: "plante03.svg", catValue: "3"},
+    {imgSrc: "table01.svg", catValue: "4"},
+    {imgSrc: "table02.svg", catValue: "4"},
+    {imgSrc: "table03.svg", catValue: "4"}
+];
