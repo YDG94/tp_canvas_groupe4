818485cf04e87fe3230722d8402810b63830f9c1
dimo <guillaume.yohann@yahoo.com>
2018-08-18 19:14:44 -0400

    Indent du code de upload.js

--- script/upload.js
+++ script/upload.js
@@ -1,27 +1,28 @@
 "use strict";
 
 // input de type file
-let inputUpload=document.querySelector('#upload');
+let inputUpload = document.querySelector('#upload');
 
 // Fonction pour lire le contenu du fichier uploade
 function readUrl() {
-    let file=document.querySelector('#upload').files[0];
-    let reader=new FileReader();
+    let file = document.querySelector('#upload').files[0];
+    let reader = new FileReader();
 
-    reader.onloadend=function () {
+    reader.onloadend = function () {
 
         $('#plan').css({
-            'background-image':`url(${reader.result})`,
+            'background-image': `url(${reader.result})`,
             'background-repeat': 'no-repeat',
             'background-size': '100% 100%',
         });
     };
 
-    if (file){
+    if (file) {
         reader.readAsDataURL(file);
-    }else{}
+    } else {
+    }
 
 }
 
 // Branchement du listener onchange
-inputUpload.addEventListener('change',readUrl,true);
+inputUpload.addEventListener('change', readUrl, true);
